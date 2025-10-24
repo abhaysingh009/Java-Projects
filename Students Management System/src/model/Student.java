823696a460ef74bfb36b36e6006a0af5cb56ef04
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2025-10-24 18:37:09 +0530

    Add files via upload

---
+++ Students Management System/src/model/Student.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [ID=" + id + ", Name=" + name + ", Age=" + age + "]";
+    }
+}
